88b5591b3b4391144a7b76931e5b079ef0644451
Vanina <winnikvanina@gmail.com>
2022-03-07 00:27:13 -0300

    Implement generic component for Icon and Text

---
+++ inmobiliaria/src/components/IconAndText.js
@@ -0,0 +1,26 @@
+import { StyleSheet, View, Text } from 'react-native'
+import React from 'react'
+import { Ionicons, FontAwesome5 } from '@expo/vector-icons'
+
+const IconAndText = ( icon ) => {
+  return (
+    <View style={styles.container}>
+      {icon.condition == 'FontAwesome5' ? <FontAwesome5 name={icon.name} size={icon.size} color={icon.color} /> : <Ionicons name={icon.name} size={icon.size} color={icon.color} />}
+      <Text style={styles.description}>{icon.description}</Text>
+    </View>
+  );
+};
+
+export default IconAndText;
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  description: {
+    fontSize: 20,
+    color: 'black',
+    marginLeft: 10,
+  },
+});
